05afb92323d45c78bddd9cbd53025c53f1de300c
hamzanth <irekpitaanthony10@gmail.com>
2025-10-01 19:32:39 +0100

    remove price and and made all model null and blank

--- courier/models.py
+++ courier/models.py
@@ -18,7 +18,7 @@ class Order(models.Model):
     tracking_number = models.CharField(max_length=10, blank=True, null=True)
     sender = models.ForeignKey("TransPerson", blank=True, null=True, on_delete=models.SET_NULL, related_name="sorder")
     reciever = models.ForeignKey("TransPerson", blank=True, null=True, on_delete=models.SET_NULL, related_name="rorder")
-    price = models.DecimalField(max_digits= 20, decimal_places=2)
+    # price = models.DecimalField(max_digits= 20, decimal_places=2)
     delivery_address = models.ForeignKey("Address", blank=True, null=True, on_delete=models.SET_NULL, related_name="dorder")
     pickup_address = models.ForeignKey("Address", blank=True, null=True, on_delete=models.SET_NULL, related_name="porder")
     shipment_date = models.DateTimeField(default=timezone.now)
@@ -40,26 +40,26 @@ class Order(models.Model):
 
 class Address(models.Model):
     # user = models.ForeignKey(User, related_name="billing_address", on_delete=models.CASCADE)
-    street_address = models.CharField(max_length=100)
-    apartment_address = models.CharField(max_length=100) 
-    country = models.CharField(max_length=100)
-    zip = models.CharField(max_length=100)
+    street_address = models.CharField(max_length=100, blank=True, null=True)
+    apartment_address = models.CharField(max_length=100, blank=True, null=True) 
+    country = models.CharField(max_length=100, blank=True, null=True)
+    zip = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f"{self.country}({self.zip})"
     
 class CurrentLocation(models.Model):
-    location_name = models.CharField(max_length=256)
-    latitude = models.CharField(max_length=256)
-    longitude = models.CharField(max_length=256)
+    location_name = models.CharField(max_length=256, blank=True, null=True)
+    latitude = models.CharField(max_length=256, blank=True, null=True)
+    longitude = models.CharField(max_length=256, blank=True, null=True)
     order = models.ForeignKey("Order", blank=True, null=True, on_delete=models.CASCADE, related_name="current_location")
 
     def __str__(self):
         return self.location_name
 
 class TransPerson(models.Model):
-    name = models.CharField(max_length=256)
-    phone_number = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, blank=True, null=True)
+    phone_number = models.CharField(max_length=256, blank=True, null=True)
     email = models.EmailField(max_length=256, blank=True, null=True)
     address = models.CharField(max_length=256, blank=True, null=True)
 
@@ -67,9 +67,9 @@ class TransPerson(models.Model):
         return self.name
     
 class Parcel(models.Model):
-    name = models.CharField(max_length=256)
-    description = models.TextField()
-    weight = models.DecimalField(max_digits=7, decimal_places=2)
+    name = models.CharField(max_length=256, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    weight = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
     length = models.DecimalField(max_digits=256, decimal_places=2, default=0)
     width = models.DecimalField(max_digits=256, decimal_places=2, default=0)
     height = models.DecimalField(max_digits=256, decimal_places=2, default=0)
